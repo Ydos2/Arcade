3a188a28e16e0fd2842a213c8cac15016dc796bf
Justine Fricou <jfricou@justine.fricou>
2021-03-23 16:43:56 +0100

    [Add] unfinished LibraryManager

---
+++ arcade_common/arcade/LibraryManager.cpp
@@ -0,0 +1,82 @@
+/*
+** EPITECH PROJECT, 2021
+** B-OOP-400-TLS-4-1-arcade-mathias.ressort
+** File description:
+** LibraryManager
+*/
+
+#include "LibraryManager.hpp"
+
+namespace arcade { // TODO
+
+    LibraryManager::LibraryManager()
+    {
+    }
+
+    LibraryManager::~LibraryManager()
+    {
+    }
+
+    void LibraryManager::load(std::string path)
+    {
+    }
+
+    void LibraryManager::loadAll(std::string path)
+    {
+    }
+
+    void LibraryManager::close(std::string name)
+    {
+    }
+
+    /* void LibraryManager::close(lib::ILibrary *lib)
+    {
+    } */
+
+    /* void LibraryManager::close(Library &lib)
+    {
+    } */
+
+    void LibraryManager::setActive(std::string name)
+    {
+    }
+
+    /* void LibraryManager::setActive(lib::ILibrary *lib)
+    {
+    } */
+
+    /* void LibraryManager::setActive(Library &lib)
+    {
+    } */
+
+    /* Library &LibraryManager::getCurrentGame() const
+    {
+    } */
+    
+    /* Library &LibraryManager::getCurrentGraphic() const
+    {
+    } */
+    
+    /* Library &LibraryManager::get(std::string name)
+    {
+    } */
+    
+    /* Library &LibraryManager::getGame(std::size_t index)
+    {
+    } */
+
+    /* Library &LibraryManager::getGraphic(std::size_t index)
+    {
+    } */
+
+    /* std::size_t LibraryManager::gameLen() const
+    {
+        return (_games.size());
+    } */
+
+    /* std::size_t LibraryManager::graphicLen() const
+    {
+        return (_graphics.size());
+    } */
+
+}
