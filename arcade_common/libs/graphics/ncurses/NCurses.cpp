aeb2e3f3fd3ad9b9e74ce133a1d0455d5c1f3051
Justine Fricou <jfricou@justine.fricou>
2021-04-06 19:51:18 +0200

    [Add] parts of NCurses' update method

--- arcade_common/libs/graphics/ncurses/NCurses.cpp
+++ arcade_common/libs/graphics/ncurses/NCurses.cpp
@@ -5,9 +5,13 @@
 ** NCurses
 */
 
-#include "NCurses.hpp"
+#include <exception>
+#include <algorithm>
 #include <iostream>
 #include <ncurses.h>
+#include "NCurses.hpp"
+#include "api/component/Transform.hpp"
+#include "api/event/KeyboardEvent.hpp"
 
 namespace arcade
 {
@@ -24,9 +28,55 @@ namespace arcade
         start_color();
     }
 
+    static std::vector<std::reference_wrapper<IEntity>> getSortedEntities(
+        IScene &scene)
+    {
+        std::vector<std::reference_wrapper<IEntity>> entities;
+
+        scene.forEach([&](IEntity& currentEnt) {
+            entities.push_back(currentEnt);
+        });
+        std::sort(entities.begin(), entities.end(), [](IEntity& a, IEntity& b) {
+            component::Transform transformA;
+            component::Transform transformB;
+
+            a.forEach([&](component::IComponent& currentComp) {
+                try {
+                    transformA = dynamic_cast<component::Transform&>(currentComp);
+                } catch (std::exception& e) {}
+            });
+            b.forEach([&](component::IComponent& currentComp) {
+                try {
+                    transformB = dynamic_cast<component::Transform&>(currentComp);
+                } catch (std::exception& e) {}
+            });
+            return (transformA.position.z > transformB.position.z);
+        });
+        return (entities);
+    }
+
     void NCurses::update(IScene &scene, float dt)
     {
+        std::vector<std::reference_wrapper<IEntity>> sortedEntities;
+        event::KeyboardEvent keyboardEvent;
+        char input = 0;
+
         std::cout << "NCurses loop" << std::endl;
+
+        // Get keyboard events
+        input = getch();
+        scene.pushEvent(/**/);
+
+        // Get mouse events
+        scene.pushEvent(/**/);
+
+        sortedEntities = getSortedEntities(scene);
+
+        // Get vector of entities' sprites
+        // ?
+
+        // Display entities' sprites
+
     }
 
     void NCurses::end(IScene &scene)
@@ -39,5 +89,7 @@ namespace arcade
     bool NCurses::quitRequested() const
     {
         std::cout << "Quit request" << std::endl;
+        curs_set(TRUE);
+        endwin();
     }
 }
